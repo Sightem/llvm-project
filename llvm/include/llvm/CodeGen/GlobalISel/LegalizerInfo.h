7f96b6316b06577a8d74227b755d9832e87089d7
Jacob Young <jacobly0@users.noreply.github.com>
2019-10-27 16:41:54 +0100

    [GlobalISel] Improve Legalizer.

--- llvm/include/llvm/CodeGen/GlobalISel/LegalizerInfo.h
+++ llvm/include/llvm/CodeGen/GlobalISel/LegalizerInfo.h
@@ -17,12 +17,15 @@
 #include "llvm/ADT/SmallBitVector.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/CodeGen/GlobalISel/LegacyLegalizerInfo.h"
+#include "llvm/CodeGen/GlobalISel/LegalizerHelper.h"
 #include "llvm/CodeGen/MachineMemOperand.h"
 #include "llvm/CodeGen/TargetOpcodes.h"
 #include "llvm/CodeGenTypes/LowLevelType.h"
 #include "llvm/MC/MCInstrDesc.h"
 #include "llvm/Support/AtomicOrdering.h"
 #include "llvm/Support/CommandLine.h"
+#include "llvm/Support/LowLevelTypeImpl.h"
+#include "llvm/Support/raw_ostream.h"
 #include <cassert>
 #include <cstdint>
 #include <tuple>
@@ -1379,6 +1382,14 @@ public:
   virtual bool legalizeCustom(LegalizerHelper &Helper, MachineInstr &MI,
                               LostDebugLocObserver &LocObserver) const {
     llvm_unreachable("must implement this if custom action is used");
+  }
+  virtual LegalizerHelper::LegalizeResult
+  legalizeCustom(MachineInstr &MI, MachineRegisterInfo &MRI,
+                 MachineIRBuilder &MIRBuilder, GISelChangeObserver &Observer,
+                 LegalizerHelper &Helper) const {
+    return legalizeCustom(Helper, MI)
+               ? LegalizerHelper::Legalized
+               : LegalizerHelper::UnableToLegalize;
   }
 
   /// \returns true if MI is either legal or has been legalized and false if not
