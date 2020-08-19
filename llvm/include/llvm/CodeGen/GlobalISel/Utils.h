44ab21da7b33137fd753810f0370584521aec6ca
Jacob Young <jacobly0@users.noreply.github.com>
2020-08-19 02:18:08 -0400

    [Combiner] Improve narrowing optimizations.

--- llvm/include/llvm/CodeGen/GlobalISel/Utils.h
+++ llvm/include/llvm/CodeGen/GlobalISel/Utils.h
@@ -231,14 +231,15 @@ struct DefinitionAndSourceRegister {
 ///
 /// Also walks through hints such as G_ASSERT_ZEXT.
 std::optional<DefinitionAndSourceRegister>
-getDefSrcRegIgnoringCopies(Register Reg, const MachineRegisterInfo &MRI);
+getDefSrcRegIgnoringCopies(Register Reg, const MachineRegisterInfo &MRI,
+                           bool HasOneNonDBGUse = false);
 
 /// Find the def instruction for \p Reg, folding away any trivial copies. May
 /// return nullptr if \p Reg is not a generic virtual register.
 ///
 /// Also walks through hints such as G_ASSERT_ZEXT.
-MachineInstr *getDefIgnoringCopies(Register Reg,
-                                   const MachineRegisterInfo &MRI);
+MachineInstr *getDefIgnoringCopies(Register Reg, const MachineRegisterInfo &MRI,
+                                   bool HasOneNonDBGUse = false);
 
 /// Find the source register for \p Reg, folding away any trivial copies. It
 /// will be an output register of the instruction that getDefIgnoringCopies
@@ -246,7 +247,8 @@ MachineInstr *getDefIgnoringCopies(Register Reg,
 /// register.
 ///
 /// Also walks through hints such as G_ASSERT_ZEXT.
-Register getSrcRegIgnoringCopies(Register Reg, const MachineRegisterInfo &MRI);
+Register getSrcRegIgnoringCopies(Register Reg, const MachineRegisterInfo &MRI,
+                                 bool HasOneNonDBGUse = false);
 
 /// Helper function to split a wide generic register into bitwise blocks with
 /// the given Type (which implies the number of blocks needed). The generic
