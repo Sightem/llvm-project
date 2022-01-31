979fd4b4b42fc925432aa9afd28711d0ad369402
Jacob Young <jacobly0@users.noreply.github.com>
2022-01-31 03:20:33 -0500

    Fix #21.

--- llvm/include/llvm/CodeGen/GlobalISel/MIPatternMatch.h
+++ llvm/include/llvm/CodeGen/GlobalISel/MIPatternMatch.h
@@ -76,7 +76,7 @@ inline std::optional<APInt> matchConstant(Register Reg,
                                           const MachineRegisterInfo &MRI) {
   if (auto ValAndVReg = getIConstantVRegValWithLookThrough(Reg, MRI))
     return ValAndVReg->Value;
-  return None;
+  return std::nullopt;
 }
 
 template <typename ConstT> struct ConstantMatch {
@@ -373,6 +373,26 @@ inline ImplicitDefMatch m_GImplicitDef() { return ImplicitDefMatch(); }
 
 // Helper for matching G_FCONSTANT
 inline bind_ty<const ConstantFP *> m_GFCst(const ConstantFP *&C) { return C; }
+
+template <typename Class> struct specific_ty {
+  Class RequestedVal;
+
+  specific_ty(Class RequestedVal) : RequestedVal(RequestedVal) {}
+
+  bool match(const MachineRegisterInfo &MRI, Register Reg) {
+    Class MatchedVal;
+    return mi_match(Reg, MRI, bind_ty<Class>(MatchedVal)) &&
+           MatchedVal == RequestedVal;
+  }
+};
+
+inline specific_ty<MachineInstr *> m_SpecificMInstr(MachineInstr *MI) {
+  return MI;
+}
+inline specific_ty<LLT> m_SpecificType(LLT Ty) { return Ty; }
+inline specific_ty<CmpInst::Predicate> m_SpecificPred(CmpInst::Predicate P) {
+  return P;
+}
 
 // General helper for all the binary generic MI such as G_ADD/G_SUB etc
 template <typename LHS_P, typename RHS_P, unsigned Opcode,
@@ -693,18 +713,6 @@ inline CompareOp_match<Pred, LHS, RHS, TargetOpcode::G_FCMP, true>
 m_c_GFCmp(const Pred &P, const LHS &L, const RHS &R) {
   return CompareOp_match<Pred, LHS, RHS, TargetOpcode::G_FCMP, true>(P, L, R);
 }
-
-// Helper for checking if a Reg is of specific type.
-struct CheckType {
-  LLT Ty;
-  CheckType(const LLT Ty) : Ty(Ty) {}
-
-  bool match(const MachineRegisterInfo &MRI, Register Reg) {
-    return MRI.getType(Reg) == Ty;
-  }
-};
-
-inline CheckType m_SpecificType(LLT Ty) { return Ty; }
 
 template <typename Src0Ty, typename Src1Ty, typename Src2Ty, unsigned Opcode>
 struct TernaryOp_match {
