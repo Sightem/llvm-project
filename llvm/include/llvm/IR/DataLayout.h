47bec45bd1860a7880020944341120b24f784127
Jacob Young <jacobly0@users.noreply.github.com>
2022-05-29 03:42:43 -0400

    Fix Z80 mangling.

--- llvm/include/llvm/IR/DataLayout.h
+++ llvm/include/llvm/IR/DataLayout.h
@@ -135,7 +135,8 @@ private:
     MM_WinCOFFX86,
     MM_GOFF,
     MM_Mips,
-    MM_XCOFF
+    MM_XCOFF,
+    MM_Z80,
   };
   ManglingModeT ManglingMode;
 
@@ -324,6 +325,7 @@ public:
       return '\0';
     case MM_MachO:
     case MM_WinCOFFX86:
+    case MM_Z80:
       return '_';
     }
     llvm_unreachable("invalid mangling mode");
@@ -332,6 +334,7 @@ public:
   StringRef getPrivateGlobalPrefix() const {
     switch (ManglingMode) {
     case MM_None:
+    case MM_Z80:
       return "";
     case MM_ELF:
     case MM_WinCOFF:
--- llvm/lib/IR/DataLayout.cpp
+++ llvm/lib/IR/DataLayout.cpp
@@ -175,6 +175,8 @@ const char *DataLayout::getManglingComponent(const Triple &T) {
     return T.getArch() == Triple::x86 ? "-m:x" : "-m:w";
   if (T.isOSBinFormatXCOFF())
     return "-m:a";
+  if (T.isZ80())
+    return "-m:z";
   return "-m:e";
 }
 
@@ -538,6 +540,9 @@ Error DataLayout::parseSpecifier(StringRef Desc) {
         break;
       case 'a':
         ManglingMode = MM_XCOFF;
+        break;
+      case 'z':
+        ManglingMode = MM_Z80;
         break;
       }
       break;
