f1fb81b2b9aaba1287c90f9d44ca1b7917579906
Jacob Young <jacobly0@users.noreply.github.com>
2022-06-03 23:46:16 -0400

    Fix some interrupt stuff.

--- llvm/include/llvm/IR/CallingConv.h
+++ llvm/include/llvm/IR/CallingConv.h
@@ -261,16 +261,18 @@ namespace CallingConv {
     /// except that the first parameter is mapped to x9.
     ARM64EC_Thunk_Native = 109,
 
+    /// Calling convention for passing an OS-defined constant in the IY register.
+    Z80_TIFlags = 110,
+
     /// Calling conventions used for special Z80 rtlib functions
     /// which pass in registers and save all registers.
-    Z80_LibCall = 110,
-    Z80_LibCall_AB = 111,
-    Z80_LibCall_AC = 112,
-    Z80_LibCall_BC = 113,
-    Z80_LibCall_L = 114,
-    Z80_LibCall_F = 115,
-    Z80_LibCall_16 = 116,
-    Z80_TIFlags = 117,
+    Z80_LibCall = 111,
+    Z80_LibCall_AB = 112,
+    Z80_LibCall_AC = 113,
+    Z80_LibCall_BC = 114,
+    Z80_LibCall_L = 115,
+    Z80_LibCall_F = 116,
+    Z80_LibCall_16 = 117,
 
     /// The highest possible calling convention ID. Must be some 2^k - 1.
     MaxID = 1023
