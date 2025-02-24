bc4dd0cff117cbcb59e5dca09aa63cc50699fb0c
Adrien Bertrand <bertrand.adrien@gmail.com>
2025-02-24 17:31:47 +0100

    fix forward-declared class usage after 02c28970

--- llvm/include/llvm/CodeGen/GlobalISel/CallLowering.h
+++ llvm/include/llvm/CodeGen/GlobalISel/CallLowering.h
@@ -21,6 +21,7 @@
 #include "llvm/CodeGen/TargetCallingConv.h"
 #include "llvm/CodeGenTypes/LowLevelType.h"
 #include "llvm/CodeGenTypes/MachineValueType.h"
+#include "llvm/IR/Attributes.h"
 #include "llvm/IR/CallingConv.h"
 #include "llvm/IR/Type.h"
 #include "llvm/IR/Value.h"
@@ -30,7 +31,6 @@
 
 namespace llvm {
 
-class AttributeList;
 class CallBase;
 class DataLayout;
 class Function;
